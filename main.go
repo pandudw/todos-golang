7d3798f911ba69aac88b29e50abe9c995f4089e2
pandudw <nasrullah.pandud1@gmail.com>
2023-12-28 20:15:10 +0700

    added post and get methods

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo"
 	"github.com/pandudw/todos-golang/database"
 )
+
+type CreateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
 
 func main() {
 	db := database.InitDB()
@@ -15,6 +24,33 @@ func main() {
 	}
 
 	e := echo.New()
+
+	e.GET("/todos", func(ctx echo.Context) error {
+		rows, err := db.Query("SELECT * FROM todos")
+		if err != nil {
+			return ctx.String(http.StatusInternalServerError, err.Error())
+		}
+
+
+	e.POST("/todos", func(ctx echo.Context) error {
+		var request CreateRequest
+		json.NewDecoder(ctx.Request().Body).Decode(&request)
+
+		_, err := db.Exec(
+			"INSERT INTO todos (title, description, done) VALUES(?, ?, 0)"
+			request.Title
+			request.Description
+		)
+
+		if err != nil {
+			return ctx.String(http.StatusInternalServerError, err.Error())
+		}
+
+		// fmt.Println(request)
+
+		return ctx.String(http.StatusOK, "OK")
+
+	})
 
 	e.Start(":8080")
 
